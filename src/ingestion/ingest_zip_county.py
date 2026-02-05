1b9fd08121c5aa469bd97cc807de80c862f5e406
Sobia005 <nskousar123@gmail.com>
2026-02-05 02:57:08 +0000

    county data

--- src/ingestion/ingest_zip_county.py
+++ src/ingestion/ingest_zip_county.py
@@ -2,43 +2,42 @@ import logging, io
 import boto3
 import pandas as pd
 import duckdb
-import os
+import os  
 from dotenv import load_dotenv
 load_dotenv()
 
 logger = logging.getLogger()
 
+def load_zip_county_data():
 # AWS session
-def load_nucc_taxonomy():
-    
     session = boto3.Session(profile_name=os.getenv("AWS_PROFILE"))
     s3 = session.resource("s3")
 
     bucket = "de-project2-nppes"
-    key = "kssnppes2/nucc_taxonomy_250.csv"
+    key = "kssnppes2/ZIP_COUNTY_032025.xlsx - Export Worksheet.csv"
 
-    # NUCC Taxonomy 250
+# NUCC Taxonomy 250
 
     obj = s3.Object(bucket, key)
     data = obj.get().get("Body").read()
-    df_nucc_taxonomy_250 = pd.read_csv(io.BytesIO(data))
+    df_zip_county = pd.read_csv(io.BytesIO(data))
 
-    # DuckDB: register df then create table
+# DuckDB: register df then create table
 
     duckdb_conn = duckdb.connect("dev.duckdb")
-    duckdb_conn.register("df_nucc_taxonomy_250", df_nucc_taxonomy_250)
-
+    duckdb_conn.register("df_zip_county", df_zip_county)
+    
     duckdb_conn.execute(" CREATE SCHEMA IF NOT EXISTS raw;")
 
+
     duckdb_conn.execute("""
-        CREATE OR REPLACE TABLE raw.nucc_taxonomy_250 AS
-        SELECT * FROM df_nucc_taxonomy_250 
+        CREATE OR REPLACE TABLE raw.zip_county AS
+        SELECT * FROM df_zip_county 
     """)
 
-    nucc_taxonomy_250 = duckdb_conn.execute("SELECT * FROM raw.nucc_taxonomy_250").fetchdf()
-    print(nucc_taxonomy_250.head(4))  
+    zip_county = duckdb_conn.execute("SELECT * FROM raw.zip_county").fetchdf()
+    print(zip_county.head())  
     duckdb_conn.close()
 
 if __name__ == "__main__":
-    load_nucc_taxonomy()
-    
+    load_zip_county_data()
